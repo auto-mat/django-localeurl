59d44ba76a9b2f98375fa2f893dabc0376de6f82
Simon Luijk <simon@simonluijk.com>
2012-04-23 14:11:29 +0200

    Patch redirect_to_login to maintain locale
    Signed-off-by: Simon Luijk <simon@simonluijk.com>

--- localeurl/models.py
+++ localeurl/models.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.core import urlresolvers
 from django.utils import translation
+from django.contrib.auth import views as auth_views
 from localeurl import utils
+
 
 def reverse(*args, **kwargs):
     reverse_kwargs = kwargs.get('kwargs') or {}
@@ -22,5 +24,25 @@ def patch_reverse():
         django_reverse = urlresolvers.reverse
         urlresolvers.reverse = reverse
 
+
+def redirect_to_login(next, login_url, *args, **kwargs):
+    if not login_url:
+        login_url = settings.LOGIN_URL
+    login_url = utils.locale_url(login_url, translation.get_language())
+    return django_redirect_to_login(next, login_url, *args, **kwargs)
+
+django_redirect_to_login = None
+
+def patch_redirect_to_login():
+    """
+    Monkey-patches the redirect_to_login function. Will not patch twice.
+    """
+    global django_redirect_to_login
+    if auth_views.redirect_to_login is not redirect_to_login:
+        django_redirect_to_login = auth_views.redirect_to_login
+        auth_views.redirect_to_login = redirect_to_login
+
+
 if settings.USE_I18N:
     patch_reverse()
+    patch_redirect_to_login()
